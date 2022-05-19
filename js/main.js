400553e9a197ee9d9a6707e33e015744c6582281
AlexVL19 <avalencia190@gmail.com>
2022-05-19 18:48:40 -0500

    Añadir bio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 const API = "https://api.github.com/users/";
 
-async function search() {
-    const response = await fetch(API + 'AlexVL19');
-    const data = await response.json();
-    console.log(data);
-}
+
+// Se cortó el async function
 
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Hola Alexis'
+            message: 'Hola Alexis',
+            searching: null,
+            result: null
         }
-    }
+    },
+    methods: { //Function ya no es necesario, ahora se usan methods.
+        async search() {
+            const response = await fetch(API + this.searching);
+            const data = await response.json();
+            console.log(data);
+            this.result = true;
+        }
+    },
+
 
 }) //Mostrar esta info de forma explícita en un elemento de HTML
