f1f16888c06548f94c890fe83797e7e54af9bdba
AlexVL19 <avalencia190@misena.edu.co>
2022-05-26 22:48:26 -0500

    Manejo de errores y visualizar resultados en el DOM

--- js/main.js
+++ js/main.js
@@ -6,17 +6,34 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Hola Alexis',
             searching: null,
-            result: null
+            result: null,
+            error: null
         }
     },
     methods: { //Function ya no es necesario, ahora se usan methods.
         async search() {
-            const response = await fetch(API + this.searching);
-            const data = await response.json();
-            console.log(data);
-            this.result = true;
+
+            this.result = this.error = null
+
+            //Insertamos un try/catch para poder enviar errores, en caso de que hubiera alguno; de lo contrario que haga lo que está abajo:
+            try {
+                const response = await fetch(API + this.searching);
+                if (!response.ok) throw new Error("Usuario no encontrado")
+                const data = await response.json();
+                console.log(data);
+                this.result = data; //Cambiar true por data para traer los datos del array que trae la API más fácil
+
+            //Arroja un error si ningún usuario existe
+            } catch (error) {
+                this.error = error
+            }
+
+            //Limpia el campo del formulario una vez se ejecuta el try/catch para encontrar a los usuarios
+            finally {
+                this.searching = null
+            }
+            
         }
     },
 
